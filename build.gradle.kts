b13278fdd39a7e7026b081348d1206c2c5a5de2a
Osanosa <22416908.co@gmail.com>
2025-03-21 05:27:16 +0200

    bump verions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    val kotlin = "2.0.20"
+    val kotlin = "2.1.20"
     id("org.jetbrains.kotlin.multiplatform") version kotlin apply false
     id("org.jetbrains.kotlin.android") version kotlin apply false
     id("org.jetbrains.kotlin.native.cocoapods") version kotlin apply false
@@ -7,12 +7,12 @@ plugins {
 
     id("org.jetbrains.kotlin.plugin.compose") version kotlin apply false
 
-    val agp = "8.5.1"
+    val agp = "8.10.0-beta01"
     id("com.android.application") version agp apply false
     id("com.android.library") version agp apply false
 
-    val compose = "1.7.0-alpha03" //Last stable: 1.6.11
+    val compose = "1.8.0-alpha04"
     id("org.jetbrains.compose") version compose apply false
 
-    id("com.google.devtools.ksp") version "$kotlin-1.0.24" apply false
+    id("com.google.devtools.ksp") version "$kotlin-1.0.31" apply false
 }
