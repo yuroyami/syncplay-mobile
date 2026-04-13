8a9fab2bf51a18ef799ef4abbd961da79d8e1730
macbook <akrame0@gmail.com>
2026-04-13 03:31:36 +0100

    Bump version

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -15,7 +15,7 @@ object AppConfig {
 
     const val appName = "Synkplay"
 
-    const val versionName = "0.19.1"
+    const val versionName = "0.20.0"
     val versionCode = ("1" + versionName.split(".").joinToString("") { it.padStart(3, '0') }).toInt()
 
     const val exoOnly = false
